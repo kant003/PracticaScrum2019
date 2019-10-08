13f5a5840c203be540a6f798cdd12f60550f3cf2
RicardoCounago <ciricardo94@gmail.com>
2019-10-08 14:03:08 +0200

    Creada funcion para saber si el numero de caracteres es par o impar

--- src/main/java/PracticasScrum/Utilidades.java
+++ src/main/java/PracticasScrum/Utilidades.java
@@ -2,4 +2,17 @@ package PracticasScrum;
 
 public class Utilidades {
 
+	public static boolean caracteresPares(String texto) {
+		
+		boolean pares = false;
+		String textoSinEspacios=texto.replaceAll("\\s","");
+		
+		if(textoSinEspacios.length()%2==0) {
+			pares=true;
+		}
+		
+		return pares;
+		
+	}
+	
 }
