2ccd608aa6fffb4d5e0a25f57a7c780f74efa4f4
tanialeee <tanialinares@gmail.com>
2019-10-08 14:17:19 +0200

    Codifico la funcionalidad de contar las letras A y hago test

--- src/main/java/PracticasScrum/Utilidades.java
+++ src/main/java/PracticasScrum/Utilidades.java
@@ -5,5 +5,17 @@ public class Utilidades {
 	void hola() {
 	
 	}
+	
+	public static int contarLetrasA(String texto) {
+		int contador=0;
+		
+		for(int i=0;i<texto.length();i++) {
+			if(texto.charAt(i)=='a'|| texto.charAt(i)== 'A') {
+				 contador ++;
+			}
+		}
+		return contador;
+		
+	}
 
 }
